b278d57cdfda5b43702e77074af59d2ff52a9c50
Fun1001 <314873178@qq.com>
2020-09-16 17:04:28 +0800

    test2

---
+++ test2/test2.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+int main()
+{
+    int i = 0;
+    int j = 0;
+    int t = 1;
+
+    for (i = 2; i <= 100; i++)
+    {
+        for (j = 2; j <= i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                t = 0;
+                break;
+            }
+        }
+        if (t == 1)
+        {
+            printf("%d\n", i);
+        }
+        t = 1;
+    }
+    return 0;
+}
